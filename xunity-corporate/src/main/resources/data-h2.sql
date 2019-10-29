f43fba32aed641d5e4bac1b1e468b40b1f5fe1b1
Adrian Q <adrianquiroz0807@gmail.com>
2019-10-29 09:09:17 -0500

    Add data to db

--- xunity-corporate/src/main/resources/data-h2.sql
+++ xunity-corporate/src/main/resources/data-h2.sql
@@ -1,1 +1,18 @@
-insert into product values (1, 'Prueba 1','Description', 15.5,20,30,1,'https://fuxionstorage.blob.core.windows.net/vhdfuxionoffix/newOffix/imageProducts/EC/EC_143392_GRA_07092018_215948_MAIN.jpg');
+insert into product values
+(1, 'FLORA LIV','¡PROTEGE TU ORGANISMO DESDE ADENTRO!',134.15,200,24,1,'https://fuxionstorage.blob.core.windows.net/vhdfuxionoffix/newOffix/imageProducts/PE/PE_142953_EXT_28062018_151551_MAIN.jpg'),
+(2, 'BEAUTY-IN','¡EL SECRETO DE LA VERDADERA BELLEZA!', 143.10,200,25,1,'https://fuxionstorage.blob.core.windows.net/vhdfuxionoffix/newOffix/imageProducts/PE/PE_143065_EXT_14122018_233843_MAIN.jpg'),
+(3, 'BIOPRO+ FIT','¡EN LA JUSTA MEDIDA!', 119.90,200,25,1,'https://fuxionstorage.blob.core.windows.net/vhdfuxionoffix/newOffix/imageProducts/PE/PE_142167_EXT_28082018_205817_MAIN.jpg'),
+(4, 'CAFÉ & CAFÉ FIT CAPPUCCINO','¡DATE UN GUSTO SIN PERDER LA LÍNEA!', 130.00,240,25,1,'https://fuxionstorage.blob.core.windows.net/vhdfuxionoffix/newOffix/imageProducts/PE/PE_141449_EXT_28062018_163943_MAIN.jpg');
+
+
+insert into customer values
+(1, 'Adrian'    ,'Quiroz'   ,'Aquiroz007'   ,'adrian'   ,999888777  ,74607070,'Av. Benavides 147', 10000,1,30,1),
+(2, 'Alonso'    ,'Loyola'   ,'Aloyola'      ,'alonso'   ,974625444  ,70805050,'Av. Benavides 150', 10000,1,35,1),
+(3, 'Christian' ,'Guerrero' ,'Cguerrero'    ,'christian',94807070   ,75495050,'Av. Benavides 197', 10000,1,2,2),
+(4, 'Juan'      ,'Perez'    ,'Jperez'       ,'juan'     ,95087070   ,75495050,'Av. Benavides 257', 10000,1,14,2);
+
+insert into user values
+(1, 'Adrian05','Quiroz','Adrian','Quiroz','aquiroz@hotmail.com','Sistemas',1),
+(2, 'Alonso41','Loyola','Alonso','Loyola','alons.20@gmail.com','Sistemas',1),
+(3, 'Christian20','Guerrero','Christian','Guerrero','cguerrero.1@gmail.com','Sistemas',1),
+(4, 'Juan21','Perez','Juan','Perez','jperez20@gmail.com','Sistemas',1);
