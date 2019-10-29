bf307cae653fa3da1b864ea7bcd03b8ade4f1a72
Adrian Q <adrianquiroz0807@gmail.com>
2019-10-29 08:43:54 -0500

    Spring Secutiry

--- xunity-corporate/src/main/java/com/xunity/xunitycorporate/Controller/LoginController.java
+++ xunity-corporate/src/main/java/com/xunity/xunitycorporate/Controller/LoginController.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping({"/", "/login"})
-    public String login() {
+    @GetMapping(value = {"/" , "/login"})
+    private String Index(){
         return "index";
+    }
+
+    @GetMapping ("/menu")
+    private String menu(){
+        return "menu";
     }
 }
---
+++ xunity-corporate/src/main/java/com/xunity/xunitycorporate/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.xunity.xunitycorporate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/","/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/menu")
+                    .failureUrl("/login?error=true")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/login?logout")
+                ;
+    }
+}
